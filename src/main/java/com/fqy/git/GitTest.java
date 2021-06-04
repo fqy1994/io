84ae25907912768ec781d29eeec39cd091204a6e
jane_glb <jane_glb@aliyun.com>
2021-06-04 18:32:49 +0800

    first commit

---
+++ src/main/java/com/fqy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.fqy.git;
+
+/**
+ * @author fan_jennifer
+ * @create 2021-06-2021/6/4 18:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hellow git");
+    }
+
+}
